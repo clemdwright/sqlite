3f55193955c21dd75e20f8f3f42b47e5c4ad5281
Clem Wright <clemw@google.com>
2014-08-28 11:17:48 -0700

    insert and select data from db

--- app/src/main/java/com/example/clemw/databasetest/MainActivity.java
+++ app/src/main/java/com/example/clemw/databasetest/MainActivity.java
@@ -1,42 +1,42 @@
 package com.example.clemw.databasetest;
 
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
 
-    VivzHelper vivzHelper;
+    EditText userName, password;
+    VivzDatabaseAdapter vivzDatabaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity);
-        vivzHelper = new VivzHelper(this);
 
-        SQLiteDatabase sqLiteDatabase = vivzHelper.getWritableDatabase();
+        userName = (EditText) findViewById(R.id.userNameValue);
+        password = (EditText) findViewById(R.id.passwordValue);
+
+        vivzDatabaseAdapter = new VivzDatabaseAdapter(this);
+
     }
 
+    public void addUser(View view) {
+        String user = userName.getText().toString();
+        String pass = password.getText().toString();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_acitivity, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        long id = vivzDatabaseAdapter.insertData(user, pass);
+        if (id < 0) {
+            Message.message(this, "Unsuccessful");
+        } else {
+            Message.message(this, "Successfully inserted a row");
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    public void viewDetails (View view) {
+        String data = vivzDatabaseAdapter.getAllData();
+        Message.message(this, data);
     }
 }
---
+++ app/src/main/java/com/example/clemw/databasetest/VivzDatabaseAdapter.java
@@ -0,0 +1,97 @@
+package com.example.clemw.databasetest;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * From: https://www.youtube.com/watch?v=tsNgtAdlof8&list=PLonJJ3BVjZW5JdoFT0Rlt3ry5Mjp7s8cT&index=13
+ */
+public class VivzDatabaseAdapter {
+
+    VivzHelper vivzHelper;
+
+    public VivzDatabaseAdapter(Context context) {
+        vivzHelper = new VivzHelper(context);
+    }
+
+    public long insertData(String name, String password) {
+        SQLiteDatabase db = vivzHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(VivzHelper.NAME, name);
+        contentValues.put(VivzHelper.PASSWORD, password);
+        long id = db.insert(VivzHelper.TABLE_NAME, null, contentValues);
+        return id;
+    }
+
+    public String getAllData() {
+        SQLiteDatabase db = vivzHelper.getWritableDatabase();
+
+        //select _id, Name, Password from VIVZTABLE
+        String[] columns = {VivzHelper.UID, VivzHelper.NAME, VivzHelper.PASSWORD};
+        Cursor cursor = db.query(VivzHelper.TABLE_NAME, columns, null, null, null, null, null);
+        StringBuffer buffer = new StringBuffer();
+        while (cursor.moveToNext()) {
+//            int index1 = cursor.getColumnIndex(VivzHelper.UID); //better way of doing this, in case column positions change
+            int cid = cursor.getInt(0); //not a robust way to do this, passing in int directly, because column positions could change
+            String name = cursor.getString(1);
+            String password = cursor.getString(2);
+            buffer.append(cid + " " + name + " " + password + "\n");
+        }
+        return buffer.toString();
+    }
+
+    static class VivzHelper extends SQLiteOpenHelper {
+        private static final String DATABASE_NAME = "vivzdatabase";
+        private static final String TABLE_NAME = "VIVZTABLE";
+        private static final String UID = "_id";
+        private static final String NAME = "Name";
+        private static final String PASSWORD = "Password";
+        private static final int DATABASE_VERSION = 4;
+        private static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + " (" +
+                UID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                NAME + " VARCHAR(255), " +
+                PASSWORD + " VARCHAR(255));";
+        private static final String DROP_TABLE = "DROP TABLE IF EXISTS " +
+                TABLE_NAME;
+        private Context context;
+
+        public VivzHelper (Context context) {
+            super (context, DATABASE_NAME, null, DATABASE_VERSION);
+            this.context = context;
+            Message.message(context, "Constructor called");
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+            try {
+                db.execSQL(CREATE_TABLE);
+                Message.message(context, "onCreate called");
+            } catch (android.database.SQLException e) {
+                e.printStackTrace();
+                Message.message(context, ""+e);
+            }
+        }
+
+        // Make sure to upgrade version number to make onUpgrade get called
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            // CMD + ALT + T to automatically surround with try catch
+            try {
+                Message.message(context, "onUpgrade called");
+                // This deletes the table
+                db.execSQL(DROP_TABLE);
+                // New database being created
+                onCreate(db);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
